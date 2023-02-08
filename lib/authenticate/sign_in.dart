d9d6bbd082902730c3eb649eae643faba0ff3fc9
puskottampandey <puskottam.pandey@gmail.com>
2023-02-08 08:28:42 +0545

    sigin UI created

--- lib/authenticate/sign_in.dart
+++ lib/authenticate/sign_in.dart
@@ -20,91 +20,103 @@ class _SignInState extends State<SignIn> {
         backgroundColor: Colors.brown.shade100,
         elevation: 0.0,
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 50),
-        child: Form(
-          child: Column(
-            children: [
-              Text(
-                "Welcome ",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.brown.shade400,
-                    fontSize: 30),
-              ),
-              const SizedBox(height: 40),
-              TextFormField(
-                cursorColor: Colors.brown.shade800,
-                cursorHeight: 30,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(32),
-                      borderSide: BorderSide(color: Colors.brown.shade800)),
-                  floatingLabelStyle: TextStyle(color: Colors.brown.shade800),
-                  labelStyle: TextStyle(color: Colors.brown.shade200),
-                  labelText: "Email",
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: (Colors.brown.shade400),
-                      ),
-                      borderRadius: BorderRadius.circular(32)),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+          child: Form(
+            child: Column(
+              children: [
+                Text(
+                  "Welcome ",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.brown.shade400,
+                      fontSize: 30),
                 ),
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              const SizedBox(
-                height: 40.0,
-              ),
-              TextFormField(
-                cursorHeight: 30,
-                cursorColor: Colors.brown.shade400,
-                obscureText: true,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(32),
-                      borderSide: BorderSide(color: Colors.brown.shade800)),
-                  floatingLabelStyle: TextStyle(color: Colors.brown.shade800),
-                  labelText: "Password",
-                  labelStyle: TextStyle(color: Colors.brown.shade200),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(32)),
-                ),
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              const Padding(padding: EdgeInsets.all(7)),
-              Text(
-                "Forget password?",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold, color: Colors.brown.shade400),
-              ),
-              const Padding(padding: EdgeInsets.all(20)),
-              SizedBox(
-                width: 150,
-                height: 40,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.brown.shade400),
+                const SizedBox(height: 40),
+                TextFormField(
+                  cursorColor: Colors.brown.shade800,
+                  cursorHeight: 30,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32),
+                        borderSide: BorderSide(color: Colors.brown.shade800)),
+                    floatingLabelStyle: TextStyle(color: Colors.brown.shade800),
+                    labelStyle: TextStyle(color: Colors.brown.shade200),
+                    labelText: "Email",
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: (Colors.brown.shade400),
+                        ),
+                        borderRadius: BorderRadius.circular(32)),
                   ),
-                  onPressed: () async {},
-                  child: const Text("Sigin"),
+                  onChanged: (val) {
+                    setState(() => email = val);
+                  },
                 ),
-              )
-            ],
+                const SizedBox(
+                  height: 40.0,
+                ),
+                TextFormField(
+                  cursorHeight: 30,
+                  cursorColor: Colors.brown.shade400,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32),
+                        borderSide: BorderSide(color: Colors.brown.shade800)),
+                    floatingLabelStyle: TextStyle(color: Colors.brown.shade800),
+                    labelText: "Password",
+                    labelStyle: TextStyle(color: Colors.brown.shade200),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(32)),
+                  ),
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  },
+                ),
+                const Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 8, horizontal: 100)),
+                Text(
+                  "Forget password?",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.brown.shade400),
+                ),
+                const Padding(padding: EdgeInsets.all(10)),
+                SizedBox(
+                  width: 150,
+                  height: 40,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Colors.brown.shade400),
+                    ),
+                    onPressed: () async {},
+                    child: const Text("Sigin"),
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.all(10)),
+                Row(
+                  children: [
+                    const Text(
+                      "Don't have a Login?",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Register",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: (Colors.brown.shade400)),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-/*style: ButtonStyle(
-  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-    RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(18.0),
-      side: BorderSide(color: Colors.red)
-    )
-  )
-)*/
